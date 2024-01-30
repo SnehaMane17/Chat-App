bba027216e892d72358702d41bf3302297f0125f
SnehaMane17 <snehamane171998@gmail.com>
2024-01-30 09:38:25 +0530

    initialized repo

---
+++ src/pages/Register.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Register = () => {
+  return (
+    <div className='formContainer'>
+        <div className='formWrapper'>
+            <form>
+                <input type='text'/>
+                <input type='email'/>
+                <input type='password'/>
+                <input type='file'/>
+            </form>
+
+        </div>
+
+    </div>
+  )
+}
